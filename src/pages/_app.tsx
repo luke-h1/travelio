5e93c2575716ce2199244479386d0d43aee64e6f
luke-h1 <luke.howsam@yahoo.com>
2022-11-24 23:17:53 +0000

    feat(frontend): add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/global.scss';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />{' '}
+    </>
+  );
 }
 
 export default App;
