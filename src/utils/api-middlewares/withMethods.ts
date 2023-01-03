3424a325552f889cf2ac3197cf9674e626105480
luke-h1 <luke.howsam@yahoo.com>
2023-01-03 11:24:57 +0000

    chore: fix tsc

--- src/utils/api-middlewares/withMethods.ts
+++ src/utils/api-middlewares/withMethods.ts
@@ -1,8 +1,6 @@
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 
-type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS' | 'HEAD';
-
-export function withMethods(methods: Method, handler: NextApiHandler) {
+export function withMethods(methods: string[], handler: NextApiHandler) {
   // eslint-disable-next-line func-names
   return async function (req: NextApiRequest, res: NextApiResponse) {
     if (!methods.includes(req.method as string)) {
